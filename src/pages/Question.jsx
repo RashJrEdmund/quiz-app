b18108ee22bf955dcdc59d8fdc0cb1e265fd041c
rash <orashusedmund@gmail.com>
2023-01-26 11:30:35 +0100

    .ad

--- src/pages/Question.jsx
+++ src/pages/Question.jsx
@@ -4,24 +4,26 @@ import { Link } from 'react-router-dom';
 
 function Question({ nextPath, pageHeader = '/question1' }) {
   return (
-    <div className="questionPage">
-      <header>{pageHeader}</header>
-      <div className="questionSepSep">this is a sample question</div>
+    <div className="question-whole">
+      <div className="questionPage">
+        <header>{pageHeader} out of 10</header>
+        <p className="questionSepSep">this is a sample question</p>
 
-      <div className="alternatives">
-        <button className="true_btn" type="button">
-          True
-        </button>
-        <button className="false" type="button">
-          False
-        </button>
+        <div className="alternatives">
+          <button className="true_btn" type="button">
+            True
+          </button>
+          <button className="false" type="button">
+            False
+          </button>
+        </div>
+
+        <Link to={nextPath}>
+          <button className="nextPage" type="button">
+            Next Question
+          </button>
+        </Link>
       </div>
-
-      <Link to={nextPath}>
-        <button className="nextPage" type="button">
-          Next Question
-        </button>
-      </Link>
     </div>
   );
 }
--- src/results/Results.jsx
+++ src/results/Results.jsx
@@ -1,22 +1,29 @@
+/* eslint-disable react/prop-types */
 import './results.css';
 import { Link } from 'react-router-dom';
 
-function Results(rights = 5, wrongs = 5) {
+function Results({ rights = 5, wrongs = 5 }) {
   return (
-    <div className="results-page">
-      <header>finalResults</header>
-      <div className="score">
-        <div className="rights">{rights}</div>
-        <div className="wrongs">{wrongs}</div>
-      </div>
-      <div className="final-total">your scored</div>
+    <div className="results-whole">
+      <div className="results-page">
+        <header>finalResults</header>
+        <div className="score">
+          <div className="rights">
+            <p>Correct</p>
+            <span>{rights}</span>
+          </div>
+          <div className="wrongs">
+            <p>Wrong</p>
+            <span>{wrongs}</span>
+          </div>
+        </div>
 
-      <div className="option">
-        would You like to retake the quize?
-        <div className="sub-options">
-          <Link to="/">
+        <p className="would-you-like">would You like to restart the quize?</p>
+
+        <div className="option">
+          <Link to="/question1">
             <button type="button" className="retake">
-              Retake
+              Restart
             </button>
           </Link>
           <Link to="/">
@@ -25,7 +32,6 @@ function Results(rights = 5, wrongs = 5) {
             </button>
           </Link>
         </div>
-        `
       </div>
     </div>
   );
